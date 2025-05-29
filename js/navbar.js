65689bca343d69f4a05f951cfed8dea30a82c08a
colocho09 <dp316941@gmail.com>
2025-05-28 21:50:57 -0600

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -14,12 +14,12 @@ class MiNavbar extends HTMLElement {
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="index.html">Inicio</a></li>
-          <li class="nav-item"><a class="nav-link" href="nosotros.html">Nosotros</a></li>
-          <li class="nav-item"><a class="nav-link" href="tienda.html">Tienda</a></li>
-          <li class="nav-item"><a class="nav-link" href="carrito.html">Carrito</a></li>
-          <li class="nav-item"><a class="nav-link" href="noticias.html">Noticias</a></li>
-          <li class="nav-item"><a class="nav-link" href="contacto.html">Contacto</a></li>
+          <li class="nav-item"><a class="nav-link" href="index.html"><i class="bi bi-house-fill"></i>Inicio</a></li>
+          <li class="nav-item"><a class="nav-link" href="nosotros.html"><i class="bi bi-people-fill"></i>Nosotros</a></li>
+          <li class="nav-item"><a class="nav-link" href="tienda.html"><i class="bi bi-bag-fill"></i>Tienda</a></li>
+          <li class="nav-item"><a class="nav-link" href="carrito.html"><i class="bi bi-cart-fill"></i>Carrito</a></li>
+          <li class="nav-item"><a class="nav-link" href="noticias.html"><i class="bi bi-newspaper"></i>Noticias</a></li>
+          <li class="nav-item"><a class="nav-link" href="contacto.html"><i class="bi bi-person-lines-fill"></i>Contacto</a></li>
           <li class="nav-item"><a class="nav-link" href="login.html" id="loginLink">Login</a></li>          
         </ul>
         <div id="userIconContainer" class="d-flex align-items-center"></div>
